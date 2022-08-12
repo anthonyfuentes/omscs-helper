3b5695579001d7e6ea7ac477003f133d630b96d3
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2022-08-11 17:44:32 -0700

    chore: scaffold project via lein new omscs

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject omscs "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns omscs.core}
+  :main omscs.core)
---
+++ src/omscs/core.clj
@@ -0,0 +1,5 @@
+(ns omscs.core)
+
+(defn -main
+  [x]
+  (println x "Hello, World!"))
