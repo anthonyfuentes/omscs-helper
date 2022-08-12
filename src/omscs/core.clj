2b7c8b1496e5977dbd0200aa380b159bdabb6213
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2022-08-12 09:08:49 -0700

    feat: update main to open course pages

--- src/omscs/core.clj
+++ src/omscs/core.clj
@@ -1,5 +1,12 @@
-(ns omscs.core)
+(ns omscs.core
+  (:require [omscs.cli :as cli]
+            [omscs.config :as conf]
+            [omscs.course-schedule :as sched]))
 
 (defn -main
-  [x]
-  (println x "Hello, World!"))
+  [& args]
+  (let [args-map (cli/args->map args)]
+    (-> (:term args-map)
+        sched/goto-term-course-search
+        sched/search-for-omscs-courses
+        (sched/open-o01-course-pages (:courses conf/all)))))
